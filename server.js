c1109b7555c682a3e1201a702579142c3a931155
Amit <109503020+amit95solanki@users.noreply.github.com>
2024-05-08 15:54:34 +0530

    updated

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const app = express();
 app.use(express.json());
 app.use(moragan("dev"));
 
+app.get("/hello", (req, res) => {
+  res.send("API Working with /api/v1");
+});
+
 //routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
